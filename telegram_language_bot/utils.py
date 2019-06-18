bd9b37e046b110468caaa78720ecaf8109790876
Danil <danil130999@gmail.com>
2019-06-18 20:09:53 +0300

    Implemented ArithmeticTime, Scheduler

--- telegram_language_bot/utils.py
+++ telegram_language_bot/utils.py
@@ -3,6 +3,16 @@
 
 
 from threading import RLock
+from language_bot_core import DBManager
+import datetime
+
+
+class SchedulerException(Exception):
+    pass
+
+
+class SchedulerPresetTypeHelperError(SchedulerException):
+    pass
 
 
 class ThreadedDict:
@@ -56,3 +66,195 @@ class ThreadedDict:
         """
         cur = self.data.__getattribute__(name)
         return self._wrap(cur)
+
+
+class ArithmeticTime(datetime.time):
+    """time class intended to support arithmetic operations and to
+    utilize already implemented datetime.time class functionality
+    """
+
+    def _normalize_time(self, hh, mm, ss):
+        h = hh
+        m = mm
+        s = ss
+
+        if s % 60 != s:
+            m += s // 60
+            s %= 60
+
+        if m % 60 != m:
+            h += m // 60
+            m %= 60
+
+        if h % 24 != h:
+            h %= 24
+        return h, m, s
+
+    def __add__(self, other: datetime.time):
+        h = self.hour + other.hour
+        m = self.minute + other.minute
+        s = self.second + other.second
+        h, m, s = self._normalize_time(h, m, s)
+        res = ArithmeticTime(h, m, s)
+        return res
+
+    def __sub__(self, other):
+        h = abs(self.hour - other.hour)
+        m = abs(self.min - other.min)
+        s = abs(self.second - other.second)
+        h, m, s = self._normalize_time(h, m, s)
+        return ArithmeticTime(h, m, s)
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            h = self.hour * other
+            m = self.minute * other
+            s = self.second * other
+            h, m, s = self._normalize_time(h, m, s)
+            return ArithmeticTime(h, m, s)
+        else:
+            raise NotImplementedError("Only integer factor allowed")
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        left_sec = self.second + self.minute * 60 + self.hour + 3600
+        right_sec = other.second + other.minute * 60 + other.hour + 3600
+        return left_sec // right_sec
+
+
+time = ArithmeticTime
+
+
+class SchedulerBase:
+    """Base class for time scheduling
+
+    """
+
+    def __init__(self, db_manager: DBManager, uid: int):
+        self.db_manager = db_manager
+        self.uid = uid
+
+    def add_time(self, time_str):
+        if self._is_valid_time_string(time_str):
+            self.db_manager.add_scheduled_time_by_uid(self.uid, time_str)
+        else:
+            raise SchedulerException('Incorrect '
+                                     'time string: {}'.format(time_str))
+
+    def add_times(self, time_str_list: list):
+        for time_str in time_str_list:
+            self.add_time(time_str)
+
+    def delete_time(self, time_str):
+        self.db_manager.delete_scheduled_time_by_uid(self.uid, time_str)
+
+    def clear_schedule(self):
+        for time_str in self.get_schedule():
+            self.delete_time(time_str)
+
+    def get_schedule(self):
+        return self.db_manager.get_schedule_by_uid(self.uid)
+
+    def _is_valid_time_string(self, time_str: str) -> bool:
+        try:
+            hh, mm, ss = time_str.split(':')
+            hh, mm, ss = int(hh), int(mm), int(ss)
+            if not 0 <= hh <= 23 \
+                    or not 0 <= mm <= 59 \
+                    or not 0 <= mm <= 59:
+                raise ValueError
+        except ValueError:
+            return False
+        return True
+
+
+class Scheduler(SchedulerBase):
+
+    allowed_preset_types = (1, 2, 3)
+
+    def add_time_from_preset(self, preset_type, min_time_str, max_time_str):
+        """
+        Rebuilds user time schedule according to given preset type and time
+        range.
+
+        :param preset_type: preset_types = {
+                                0: 'Every half an hour',
+                                1: 'Every hour',
+                                2: 'Every two hours',
+                            }
+        :param min_time_str:
+        :param max_time_str:
+        :return:
+        """
+        self.clear_schedule()
+        if preset_type not in self.allowed_preset_types:
+            raise SchedulerException('Incorrect preset type.')
+        else:
+            attr_name = "_handle_type{}_preset".format(preset_type)
+            schedule_gen_method = getattr(self, attr_name, lambda *x: None)
+            schedule = schedule_gen_method(min_time_str, max_time_str)
+            if schedule is None:
+                raise SchedulerPresetTypeHelperError('An error occurred while'
+                                                     'handling preset type')
+        self.clear_schedule()
+        self.add_times(schedule)
+
+    def _handle_type1_preset(self, min_time_str, max_time_str) -> list:
+        """
+        Build type one (one msg per half an hour) time schedule preset
+        in given time range
+
+        :param min_time_str:
+        :param max_time_str:
+        :return:
+        """
+        delta_time_str = "00:30:00"
+        return self.base_preset_handler(min_time_str,
+                                        max_time_str,
+                                        delta_time_str)
+
+    def _handle_type2_preset(self, min_time_str, max_time_str) -> list:
+        """
+        Build type two (one msg per hour) time schedule preset
+        in given time range
+
+        :param min_time_str:
+        :param max_time_str:
+        :return:
+        """
+        delta_time_str = "01:00:00"
+        return self.base_preset_handler(min_time_str,
+                                        max_time_str,
+                                        delta_time_str)
+
+    def _handle_type3_preset(self, min_time_str, max_time_str) -> list:
+        """
+        Build type one (one msg per two hours) time schedule preset
+        in given time range
+
+        :param min_time_str:
+        :param max_time_str:
+        :return:
+        """
+        delta_time_str = "02:00:00"
+        return self.base_preset_handler(min_time_str,
+                                        max_time_str,
+                                        delta_time_str)
+
+    def base_preset_handler(self, min_time_str, max_time_str, delta_time_str):
+        start = ArithmeticTime.fromisoformat(min_time_str)
+        stop = ArithmeticTime.fromisoformat(max_time_str)
+        delta = ArithmeticTime.fromisoformat(delta_time_str)
+        return [start + i * delta for i in range((start - stop)/delta)]
+
+
+
+if __name__ == '__main__':
+    s = ArithmeticTime(0, 0, 45)
+    for i in range(10):
+        print(i * s, end='\t')
+        t = i * s
+        print(t / s)
+
